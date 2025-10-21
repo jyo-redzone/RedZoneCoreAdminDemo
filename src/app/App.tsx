262645b6b7c85c3bcd3cc452dd8e2ee62ecfcb7e
jyo-redzone <jyellumahanti@redzonetechnology.com>
2025-10-21 14:45:48 +0530

    Updates

--- src/app/App.tsx
+++
@@ -1,5 +0,0 @@
-import { AdminPage } from '../features/admin/AdminPage';
-
-export default function App() {
-  return <AdminPage />;
-}
--- src/shared/utils/ids.ts
+++
@@ -1,7 +0,0 @@
-export const generateId = (): string => {
-  return `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
-};
-
-export const isValidId = (id: string): boolean => {
-  return typeof id === 'string' && id.length > 0;
-};
